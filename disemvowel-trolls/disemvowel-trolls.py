4c1f7602625da41688d9d0c37509dcb9558fe5f7
Festus Kogo <fkogoson@gmail.com>
2024-01-03 23:04:01 +0300

    Add disemvowel-trolls/disemvowel-trolls.py challenge

---
+++ disemvowel-trolls/disemvowel-trolls.py
@@ -0,0 +1,32 @@
+# https://www.codewars.com/kata/52fba66badcd10859f00097e/train/python
+
+# Trolls are attacking your comment section!
+
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+# Note: for this kata y isn't considered a vowel.
+
+def remove_vowels(s):
+    vowels = ["a", "e", "i", "o", "u"]
+    s = list(s) # list
+    arr = []
+
+    for letter in s:
+        if letter == "":
+            arr.append(" ")
+        
+        elif letter.lower() not in vowels:
+            arr.append(letter)
+
+    return "".join(arr)
+
+
+
+print(remove_vowels("This website is for losers LOL!"))
+print(remove_vowels("Hey dude"))
+print(remove_vowels("Heeey"))
+print(remove_vowels("Heeooooooooooooiiiiiiiiiiiiiiy"))
