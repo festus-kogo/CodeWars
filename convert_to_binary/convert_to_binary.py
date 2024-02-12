6c01c07d8ae92184f4d82034a489aa859f5e4230
Festus Kogo <fkogoson@gmail.com>
2024-02-12 07:25:21 +0300

    Update convert_to_binary.py add to_decimal.py function

--- convert_to_binary/convert_to_binary.py
+++ convert_to_binary/convert_to_binary.py
@@ -16,5 +16,28 @@ print(f"{to_binary(1)}") # 1
 print(f"{to_binary(2)}") # 10
 print(f"{to_binary(3)}") # 11
 print(f"{to_binary(4)}") # 100
+print(f"{to_binary(128)}") # 10000000
+
+def to_decimal(b):
+    dec = [128, 64, 32, 16, 8, 4, 2, 1]
+    m_sum = 0
+
+    if len(list(str(b))) != 8:
+        l = len(list(str(b)))
+        b = "0" * (8 - l) + str(b)
+        
+    for i, v in enumerate(list(str(b))):
+        if int(v) == 1:
+            m_sum += dec[i]
+    
+    return m_sum
+
+print(f"{to_decimal(1)}") # 1
+print(f"{to_decimal(10)}") # 2
+print(f"{to_decimal(11)}") # 3
+print(f"{to_decimal(100)}") # 4
+print(f"{to_decimal(10000000)}") # 128
+
+
 
 
