b68970eb65618b697394282da79b66e4759672c9
Festus Kogo <fkogoson@gmail.com>
2024-04-02 09:09:18 +0300

    Add Javascript array filter challenge

---
+++ javascript_array_filter/javascript_array_filter.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/514a6336889283a3d2000001/train/python
+
+def get_even_numbers(arr):
+    
+    arr2 = [element for element in arr if element % 2 == 0]
+    
+    return arr2
